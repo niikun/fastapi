89098fb93138cab06bfebd9c6312742efb49be25
niikun <niikun0209@gmail.com>
2024-05-20 13:53:40 +0000

    fastapi update

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ async def root():
 
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
-    """Add two numbers together"""
+    """
+    Add two numbers together
+    """
 
     total = num1 + num2
     return {"total": total}
